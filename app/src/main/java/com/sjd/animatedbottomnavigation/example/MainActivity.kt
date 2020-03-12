ec314d1d9299207e520a90d3b1fda22042277323
sjd753 <sjd753@gmail.com>
2020-03-12 17:07:34 +0530

    added window decor and color frame

--- app/src/main/java/com/sjd/animatedbottomnavigation/example/MainActivity.kt
+++ app/src/main/java/com/sjd/animatedbottomnavigation/example/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.sjd.animatedbottomnavigation.example
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.sjd.animatedbottomnavigation.R
 import com.sjd.animatedbottomnavigation.library.OnItemSelectedListener
@@ -13,9 +16,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = Color.TRANSPARENT
+            window.navigationBarColor = Color.TRANSPARENT
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility += View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+
+        val colors = resources.getIntArray(R.array.menuItemColors)
+
         bottomNavigationBar.setOnItemSelectedListener(object : OnItemSelectedListener {
             override fun onItemSelect(position: Int) {
-
+                appBarLayout.setBackgroundColor(colors[position])
+                frameLayout.setBackgroundColor(colors[position])
             }
         })
     }
